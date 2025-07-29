97e69d6278cf5aa1c323c9cdd53fdef45b973e40
git_admin <admin@admin.admin>
2025-07-29 15:54:51 +0300

    proxy format fix

--- capmonstercloud_client/requests/TurnstileRequest.py
+++ capmonstercloud_client/requests/TurnstileRequest.py
@@ -1,10 +1,9 @@
 from typing import Dict, Optional
 from pydantic import Field, validator, model_validator
-from .baseRequestWithProxy import BaseRequestWithProxy
+from .baseRequestWithProxy import BaseRequestWithProxy, BaseRequestsCloudFlare
 
 
-class TurnstileRequest(BaseRequestWithProxy):
-    
+class TurnstileRequest(BaseRequestsCloudFlare):
     type: str = Field(default="TurnstileTask")
     websiteURL: str
     websiteKey: str
@@ -23,29 +22,28 @@ class TurnstileRequest(BaseRequestWithProxy):
                 raise ValueError(f'cloudflareTaskType could be "cf_clearance" if you need cookie or ' \
                                  f'"token" if required token from Turnstile.')
         return value
-    
+
     @model_validator(mode='before')
     def validate_cloudflare_type_token(self):
-        
+
         if self.get('htmlPageBase64') is None:
             if self.get('cloudflareTaskType') == 'cf_clearance':
                 raise RuntimeError(f'Expect that "htmlPageBase64" will be filled ' \
-                    f'when cloudflareTaskType is "cf_clearance"')
-        
+                                   f'when cloudflareTaskType is "cf_clearance"')
+
         if self.get('cloudflareTaskType') == 'token':
             for field in ['pageAction', 'pageData', 'data']:
                 if self.get(field) is None:
                     raise RuntimeError(f'Expect that "{field}" will be filled ' \
-                    f'when "cloudflareTaskType" = "token".')
-        
+                                       f'when "cloudflareTaskType" = "token".')
+
         if self.get('cloudflareTaskType') is not None:
             if self.get('cloudflareTaskType') in ['cf_clearance', 'token']:
                 if self.get('userAgent') is None:
                     raise RuntimeError(f'Expect that userAgent will be filled ' \
-                        f'when cloudflareTaskType specified.')
-                    
+                                       f'when cloudflareTaskType specified.')
+
         return self
-    
+
     def getTaskDict(self) -> Dict[str, str]:
         return {k: v for k, v in self.model_dump().items() if v is not None}
-    
--- capmonstercloud_client/requests/baseRequestWithProxy.py
+++ capmonstercloud_client/requests/baseRequestWithProxy.py
@@ -5,3 +5,10 @@ from .proxy_info import ProxyInfo
 
 class BaseRequestWithProxy(BaseRequest):
     proxy: Optional[ProxyInfo] = None
+
+class BaseRequestsCloudFlare(BaseRequest):
+    proxyType: Optional[str] = None
+    proxyAddress: Optional[str] = None
+    proxyPort: Optional[int] = None
+    proxyLogin: Optional[str] = None
+    proxyPassword: Optional[str] = None
